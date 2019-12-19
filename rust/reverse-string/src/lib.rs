601a9d81f114f48ba48a8fa7dd7e1376dbdc055e
Matt Nelson <menelso2@illinois.edu>
2019-12-18 18:28:52 -0600

    remove deprecated 'extern crate' and reduce dependencies

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,7 @@
+extern crate unicode_segmentation;
+
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    return input.chars().rev().collect();
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
 }
